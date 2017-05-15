3ab4b4819210654d6582fe159d1d47741a15d019
Sami <sami@nieminen.fi>
2017-05-15 20:31:38 +0300

    updated eslint and some packages

--- src/components/AuthBanner.js
+++ src/components/AuthBanner.js
@@ -1,23 +1,26 @@
+/* eslint no-shadow: 0*/
+/* eslint-env es6*/
+
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { logout } from '../actions/authActions'
 
 const AuthBanner = ({ userName, logout }) => (
-    <div>
-        <h3>{userName}</h3>
-        <button className="btn btn-warning btn-xs" type="button" onClick={logout} >Kirjaudu ulos</button>
-    </div>
+  <div>
+    <h3>{userName}</h3>
+    <button className="btn btn-warning btn-xs" type="button" onClick={logout} >Kirjaudu ulos</button>
+  </div>
 )
 
 AuthBanner.propTypes = {
-    userName: PropTypes.string.isRequired,
-    logout: PropTypes.func.isRequired
+  userName: PropTypes.string.isRequired,
+  logout: PropTypes.func.isRequired
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   userName: state.auth.userName
 })
 
 export default connect(mapStateToProps, {
   logout
-})(AuthBanner);
+})(AuthBanner)
--- tools/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    dbUri: "mongodb://localhost/niemisami_db",
-    jwtSecret: "hyshyssalaisuudet"
-}
